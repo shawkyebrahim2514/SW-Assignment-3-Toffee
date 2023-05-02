5074bf2df9c0681dff94fbd943cc0a3e9b0a2476
AdhamMahmoud1 <adhammahmoud0163@gmail.com>
2023-05-02 20:57:38 +0300

    styling the classes of shipping cart

--- project/com/ShippingCart/Payment.java
+++ project/com/ShippingCart/Payment.java
@@ -1,5 +1,21 @@
 package com.ShippingCart;
 
+import com.User.*;
+
 public class Payment {
+    private User CurrentBuyer;
+
+    private void payByCash(){}
+
+    private void payBySmartWallet(){}
+
+    private void payByGiftVoucher(){}
+
+    private void payByLoyalityPoints() {}
     
+    private void chooseAddress() {
+    }
+    
+    public void pay(double cost, String type){}
+
 }
