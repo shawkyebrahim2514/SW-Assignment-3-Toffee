a57192837ca08ed3ff9c6a1bddb9b35f04cf2de4
Shawky Ebrahim Ahmed <101745968+shawkyebrahim2514@users.noreply.github.com>
2023-05-03 11:47:12 +0300

    Upload manageItems, manageUsers structure

--- project/com/System/ManageItems.java
+++ project/com/System/ManageItems.java
@@ -1,5 +1,42 @@
 package com.System;
 
+import com.ShippingCart.Item;
+
 public class ManageItems {
-    
+    private Item[] items;
+
+    // Methods for Users
+    public void search() {
+    }
+
+    public void viewCatalog() {
+    }
+
+    public void chooseItem() {
+    }
+
+    // Methods for Admins
+    public void addItems() {
+    }
+
+    public void deleteItems() {
+    }
+
+    public void updateItems() {
+    }
+
+    public void updateCatalog() {
+    }
+
+    public void viewOrders() {
+    }
+
+    public void setLoyaltyPoints() {
+    }
+
+    public void putRestrictions() {
+    }
+
+    public void viewStatistics() {
+    }
 }
--- project/com/System/ManageUsers.java
+++ project/com/System/ManageUsers.java
@@ -1,5 +1,16 @@
 package com.System;
 
+import com.User.Admin;
+import com.User.Buyer;
+
 public class ManageUsers {
-    
+    private Buyer[] buyers;
+    private Admin[] admins;
+    public void checkExistBuyer(){}
+    public void checkExistUser(){}
+    public void addNewUser(){} // revise it
+    public void accessAdmin(){}
+    public void suspendUser(){}
+
+
 }
